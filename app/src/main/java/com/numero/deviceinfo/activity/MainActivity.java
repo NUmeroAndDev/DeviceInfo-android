ad68dd18380c480328241145683b7e43edf17452
NUmeroAndDev <numero.dev.and@gmail.com>
2017-09-28 22:15:22 +0900

    画面の表示処理を追加

--- app/src/main/java/com/numero/deviceinfo/activity/MainActivity.java
+++ app/src/main/java/com/numero/deviceinfo/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.numero.deviceinfo.R;
+import com.numero.deviceinfo.fragment.DeviceInfoFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getFragmentManager().beginTransaction().replace(R.id.container, DeviceInfoFragment.getInstance()).commit();
     }
 }
--- app/src/main/java/com/numero/deviceinfo/fragment/DeviceInfoFragment.java
+++ app/src/main/java/com/numero/deviceinfo/fragment/DeviceInfoFragment.java
@@ -1,17 +1,39 @@
 package com.numero.deviceinfo.fragment;
 
+import android.app.Fragment;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
 import android.support.annotation.Nullable;
 
 import com.numero.deviceinfo.R;
+import com.numero.deviceinfo.model.DeviceInfo;
 
 public class DeviceInfoFragment extends PreferenceFragment {
+
+    private DeviceInfo deviceInfo = new DeviceInfo();
+
+    public static Fragment getInstance() {
+        return new DeviceInfoFragment();
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.device_info_list);
-        
+
+        setupLayout();
+    }
+
+    private void setupLayout() {
+        PreferenceScreen deviceNameScreen = (PreferenceScreen) findPreference("device_name_screen");
+        PreferenceScreen deviceBrandScreen = (PreferenceScreen) findPreference("device_brand_screen");
+        PreferenceScreen deviceVersionScreen = (PreferenceScreen) findPreference("device_version_screen");
+        PreferenceScreen deviceCodeNameScreen = (PreferenceScreen) findPreference("device_code_name_screen");
+
+        deviceNameScreen.setSummary(deviceInfo.getName());
+        deviceBrandScreen.setSummary(deviceInfo.getBrand());
+        deviceVersionScreen.setSummary(deviceInfo.getVersion());
+        deviceCodeNameScreen.setSummary(deviceInfo.getCodeName());
     }
 }
